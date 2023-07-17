308e34cff42803966ca934a90a0d9f6c996193d3
Eugénie <eugenie.khayat@gmail.com>
2023-07-17 23:07:29 +0200

    render all books in category all

--- components/BooksCategories.jsx
+++ components/BooksCategories.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import { CATEGORIES, LIVRES } from '../models/data';
 
 export default function BooksCategories() {
+  const { categorieId } = useRoute().params;
+
+  if (categorieId === undefined) {
+    return (
+      <View>
+        <Text>Error: Catégorie non spécifiée</Text>
+      </View>
+    );
+  }
+  const filteredBooks = categorieId === 'c5' ? LIVRES : LIVRES.filter((livre) => livre.categorieId.includes(categorieId));
   return (
-    <Text>TEST</Text>  )
+    <View style={styles.container}>
+      <Text style={styles.categoryTitle}>{CATEGORIES.genre}</Text>
+      <FlatList
+        data={filteredBooks}
+        renderItem={({ item }) => (
+          <View style={styles.bookContainer}>
+            <Text style={styles.bookTitle}>{item.titre}</Text>
+            {/* Vous pouvez afficher d'autres détails du livre ici */}
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  categoryTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  bookContainer: {
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    paddingBottom: 10,
+    borderBottomColor: '#ccc',
+  },
+  bookTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import { CATEGORIES } from '../models/data';
+import { LIVRES } from '../models/data';
 
 export default function Categories() {
 
-  const route = useRoute();
+  const navigation = useNavigation();
+
+  const handleCategoryPress = (categorie) => {
+    if (categorie.id === 'c5') {
+      // Pour la catégorie "Tous", nous passons tous les livres sans filtre
+      navigation.navigate('BooksCategories', { categorieId: 'c5' });
+    } else {
+      // Pour les autres catégories, nous passons l'ID de la catégorie sélectionnée
+      navigation.navigate('BooksCategories', { categorieId: categorie.id });
+    }
+  };
+  
 
   const renderCategoryButton = ({ item }) => (
-    <TouchableOpacity style={[styles.categoryButton, { backgroundColor: item.couleur }]}>
+    <TouchableOpacity
+      style={[styles.categoryButton, { backgroundColor: item.couleur }]}
+      onPress={() => handleCategoryPress(item)}
+    >
       <Text style={styles.categoryText}>{item.genre}</Text>
       <Text style={styles.categoryText}>{item.couleur}</Text>
     </TouchableOpacity>
   );
-
   return (
     <FlatList
       data={CATEGORIES}
@@ -22,7 +36,6 @@ export default function Categories() {
     />
   );
 }
-
 const styles = StyleSheet.create({
   categoryButton: {
     padding: 10,
