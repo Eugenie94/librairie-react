abd7a91e632526e083417810251d0816476a56df
Eugénie <eugenie.khayat@gmail.com>
2023-07-17 22:29:31 +0200

    render books on my component

--- components/Books.jsx
+++ components/Books.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import { useRoute } from '@react-navigation/native';
-import { Text } from 'react-native';
+import { LIVRES } from '../models/data';
 
 export default function Books() {
+  
+const route = useRoute();
 
-    const route = useRoute();
+const renderLivre = ({ item }) => (
+    <View key={item.id} style={styles.livreContainer}>
+      <Text style={styles.titre}>{item.titre}</Text>
+      <Text style={styles.description}>{item.description}</Text>
+      <Text style={styles.tomes}>Tomes: {item.tomes}</Text>
+    </View>
+  );
 
   return (
-    <Text>Books</Text>
-  )
+    <FlatList
+    data={LIVRES}
+    renderItem={renderLivre}
+    keyExtractor={(item) => item.id}
+  />
+  );
 }
+  const styles = StyleSheet.create({
+    livreContainer: {
+      marginVertical: 10,
+      padding: 10,
+      borderWidth: 1,
+      borderColor: '#ccc',
+    },
+    titre: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    description: {
+      fontSize: 16,
+      color: '#888',
+    },
+    tomes: {
+      fontSize: 14,
+      color: '#444',
+    },
+  });
